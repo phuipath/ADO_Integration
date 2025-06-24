32a7503b30e61867cc6da16d498c9e77b8529d64
Phil <Phil.Harris@uipath.com>
2025-06-24 09:03:08 -0500

    Committed to DevOps

--- SAP FI/.codedworkflows/ConnectionsFactory.cs
+++ SAP FI/.codedworkflows/ConnectionsFactory.cs
@@ -19,11 +19,8 @@ namespace PublicSectorDemo
 
     public class GmailFactory
     {
-        public UiPath.GSuite.Activities.Api.GmailConnection SandBox_Rahul_Sridharan_uptestsuite_gmail_com { get; set; }
-
         public GmailFactory(ICodedWorkflowsServiceContainer resolver)
         {
-            SandBox_Rahul_Sridharan_uptestsuite_gmail_com = new UiPath.GSuite.Activities.Api.GmailConnection("03bdd54f-9541-43fd-8c58-ee42cbcf4c9a", resolver);
         }
     }
 
